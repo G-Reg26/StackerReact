66bd1c1afe0cb2191e7e3b8b4f5fca6391326c7d
Gregorio Lozada <greglozada@gmail.com>
2019-08-19 01:36:28 -0400

    Updates

---
+++ precache-manifest.ba10d765aad8837c7bfc51b21b2ac684.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743f235a015171b90e68524d5ba1734c",
+    "url": "/G-Reg26/StackerReact/index.html"
+  },
+  {
+    "revision": "c11b3b8f976299b465df",
+    "url": "/G-Reg26/StackerReact/static/css/main.41a649f1.chunk.css"
+  },
+  {
+    "revision": "3e02a8c67cfbd25b4d1a",
+    "url": "/G-Reg26/StackerReact/static/js/2.1b5bec0c.chunk.js"
+  },
+  {
+    "revision": "c11b3b8f976299b465df",
+    "url": "/G-Reg26/StackerReact/static/js/main.2e80a71b.chunk.js"
+  },
+  {
+    "revision": "05b0e2830dabe0ff7e78",
+    "url": "/G-Reg26/StackerReact/static/js/runtime~main.dee90e9d.js"
+  }
+]);
